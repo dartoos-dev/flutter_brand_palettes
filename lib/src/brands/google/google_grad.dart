70849256277bfc0d14e73637c3ec145f2eed0845
rafael <r.amizes@gmail.com>
2021-06-25 15:10:28 -0300

    chore: add coverage suppressing statements

--- lib/src/brands/google/google_grad.dart
+++ lib/src/brands/google/google_grad.dart
@@ -16,12 +16,14 @@ class GoogleGrad extends SwatchBase {
   ///
   /// [google-color-sequence](https://www.behance.net/gallery/9028077/Google-Visual-Assets-Guidelines-Part-1)
   const GoogleGrad()
+      // coverage:ignore-start
       : super(const [
           Google.blue(),
           Google.red(),
           Google.yellow(),
           Google.green(),
         ]);
+  // coverage:ignore-end
 
   @override
   List<Color> get colors => List.unmodifiable(super.colors);
